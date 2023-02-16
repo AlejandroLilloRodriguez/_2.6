373cfa8ceaa52bba2f07c4a6ca0306c217d0cc0c
AlejandroLilloRodriguez <alejandro.lillorodriguez@usp.ceu.es>
2023-02-16 15:48:37 +0100

    Ejercicio6 hecho

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,8 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import dominio.*;
+public class Principal {
+    public static void main (String[] args){
+        int[] lista={1,3,4,5,7};
+        System.out.print(Ejercicio6.desviaciontipica(lista));
+    }
 }
--- src/dominio/Ejercicio6.java
+++ src/dominio/Ejercicio6.java
@@ -1,2 +1,25 @@
-package dominio;public class Ejercicio6 {
+package dominio;
+
+public class Ejercicio6 {
+    public static int suma(int[] lista, int n) {
+        if (n == 0)
+            return lista[0];
+        else
+            return lista[n] + suma(lista, n - 1);
+    }
+
+    public static int alcuadrado(int[] lista, int n) {
+        if (n == 0)
+            return lista[0] * lista[0];
+        else
+            return lista[n] * lista[n] + alcuadrado(lista, n - 1);
+    }
+
+    public static double desviaciontipica(int[] lista) {
+        double alcuadrado = alcuadrado(lista, lista.length - 1);
+        double media = suma(lista, lista.length - 1) / lista.length;
+        double varianza = (alcuadrado / lista.length) - media * media;
+        double desviaciontipica = Math.sqrt(varianza);
+        return desviaciontipica;
+    }
 }
